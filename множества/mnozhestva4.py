f13d18e10c77b60fd1b6aa625aac24728e7edf73
RylyakovaDasshha <146470647+RylyakovaDasshha@users.noreply.github.com>
2023-12-17 21:01:27 +0700

    Update mnozhestva4.py

--- множества/mnozhestva4.py
+++ множества/mnozhestva4.py
@@ -1,10 +1,15 @@
+#вводим последовательность чисел через пробел 
 sequence = input("Введите числа через пробел: ")
+#разбиваем пос-во чисел на отдельные числа
 numbers = sequence.split()
+#создаем множества
 previous_numbers = set()
-
+#проходимся по списку numbers
 for number in numbers:
+    #если число раньше встречалось выводится да
     if number in previous_numbers:
         print("YES")
+        #если число не встречалось раньше выводится нет
     else:
         print("NO")
         previous_numbers.add(number)
